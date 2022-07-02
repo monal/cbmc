a570327a4778a94a887c3548e3848691db24d17b
Daniel Kroening <dkr@amazon.com>
2022-07-02 21:57:22 +0100

    add may_be_same_object

---
+++ src/cprover/may_be_same_object.cpp
@@ -0,0 +1,20 @@
+/*******************************************************************\
+
+Module: May Be Same Object
+
+Author:
+
+\*******************************************************************/
+
+/// \file
+/// May Be Same Object
+
+#include "may_be_same_object.h"
+
+optionalt<exprt> may_be_same_object(
+  const exprt &a,
+  const exprt &b,
+  const std::unordered_set<symbol_exprt, irep_hash> &address_taken,
+  const namespacet &ns)
+{
+}
